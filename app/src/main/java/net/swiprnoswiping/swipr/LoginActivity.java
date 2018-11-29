725bdabd500c6a40313e5d629665a3452ea7be06
ajf500 <ajf500@nyu.edu>
2018-11-29 18:11:40 -0500

    super secret login

--- app/src/main/java/net/swiprnoswiping/swipr/LoginActivity.java
+++ app/src/main/java/net/swiprnoswiping/swipr/LoginActivity.java
@@ -3,6 +3,7 @@ package net.swiprnoswiping.swipr;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -93,6 +94,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+    }
+
+    public void goToHome(View view){
+        Intent homeIntent = new Intent(this, HomePage.class);
+        startActivity(homeIntent);
     }
 
     private void populateAutoComplete() {
