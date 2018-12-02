bd74ec45613a2db584b2cae3153a5d4e42a1d15f
Parina <time-is-immortal@users.noreply.github.com>
2018-12-01 20:52:59 -0500

    Added map

--- app/src/main/java/net/swiprnoswiping/swipr/HomePage.java
+++ app/src/main/java/net/swiprnoswiping/swipr/HomePage.java
@@ -39,7 +39,7 @@ public class HomePage extends AppCompatActivity {
     }
 
     public void requestButton(View view){
-        Intent requestIntent = new Intent(this, MapActivity.class);
+        Intent requestIntent = new Intent(this, MapsActivity.class);
         startActivity(requestIntent);
     }
 }
