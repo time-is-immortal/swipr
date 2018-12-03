0da8ff2976402eabea5a5a750bdbbbe9946c5de8
Parina <time-is-immortal@users.noreply.github.com>
2018-12-02 21:02:21 -0500

    removed mail button from login screen

--- app/src/main/java/net/swiprnoswiping/swipr/Test.java
+++ app/src/main/java/net/swiprnoswiping/swipr/Test.java
@@ -25,14 +25,6 @@ public class Test extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
