d153e36d678bef7fea508546f09c35542b121a0b
Ammy <al4604@nyu.edu>
2018-12-04 16:06:44 -0500

    manage friend request page

--- app/src/main/java/net/swiprnoswiping/swipr/manageFriendRequests.java
+++ app/src/main/java/net/swiprnoswiping/swipr/manageFriendRequests.java
@@ -5,22 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class FriendActivity extends AppCompatActivity {
+public class manageFriendRequests extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_friend);
-    }
-
-    public void friendsList(View view){
-        Intent fLIntent = new Intent(this, FriendsListActivity.class);
-        startActivity(fLIntent);
-    }
-
-    public void manageFriendRequests(View view){
-        Intent manageIntent = new Intent(this, manageFriendRequests.class);
-        startActivity(manageIntent);
+        setContentView(R.layout.activity_manage_friend_requests);
     }
 
     public void backButton(View view){
