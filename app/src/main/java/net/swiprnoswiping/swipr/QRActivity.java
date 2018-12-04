d9a09e843e909a9e571047390712a13956b8fbc3
Parina <time-is-immortal@users.noreply.github.com>
2018-12-04 15:30:34 -0500

    fixed merge

--- app/src/main/java/net/swiprnoswiping/swipr/QRActivity.java
+++ app/src/main/java/net/swiprnoswiping/swipr/QRActivity.java
@@ -2,13 +2,24 @@ package net.swiprnoswiping.swipr;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
 import com.google.zxing.BarcodeFormat;
-import com.journeyapps.barcodescanner.BarcodeEncoder;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 
 public class QRActivity extends AppCompatActivity {
@@ -17,18 +28,44 @@ public class QRActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
+    final RelativeLayout rl = (RelativeLayout) findViewById(R.id.reLay);
+    final ImageView qrcode = (ImageView) findViewById(R.id.qrCode);
+    final String random_string = "randomSecretConfirmationKey";
+    Button btn = (Button) findViewById(R.id.scanME);
+    btn.setOnClickListener(new View.OnClickListener(){
+       @Override
+       public void onClick(View v) {
+           try {
+               Bitmap temp;
+               temp = genCode(random_string);
+               qrcode.setImageBitmap(temp);
+           }
+           catch (WriterException e) {
+               System.out.println("God is cruel");
+           }
+           catch (IOException e){
+               System.out.println("Please kill me");
+           }
+       }
+    });
     }
 
-    try {
-        String random_string = "randomSecretConfirmationKey"
-        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-        Bitmap bitmap = barcodeEncoder.encodeBitmap(random_string, BarcodeFormat.QR_CODE, 400, 400);
-        ImageView imageViewQrCode = (ImageView) findViewById(R.id.qrCode);
-        imageViewQrCode.setImageBitmap(bitmap);
-    } catch(Exception e) {
-
+    private Bitmap genCode(String text) throws WriterException, IOException {
+        int width = 400;
+        int height = 400;
+        QRCodeWriter writer = new QRCodeWriter();
+        BitMatrix bitMatrix = writer.encode(text, BarcodeFormat.QR_CODE, width, height);
+//        ByteArrayOutputStream pngStream = new ByteArrayOutputStream();
+//        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngStream);
+//        byte [] pngData = pngStream.toByteArray();
+        Bitmap qrcode = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        for (int x = 0; x < width; x++){
+            for (int y = 0; y < height; y++){
+                qrcode.setPixel(x, y, bitMatrix.get(x,y) ? Color.BLACK : Color.WHITE);
+            }
+        }
+        return qrcode;
     }
-
 
 
     public void backButton(View view){
