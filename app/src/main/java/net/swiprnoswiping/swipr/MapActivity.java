584e378e2473983a8a29c512a71277234e38c639
Parina <time-is-immortal@users.noreply.github.com>
2018-12-04 18:39:26 -0500

    Deleted ScanActivity, ScanIntent in MapActivity

--- app/src/main/java/net/swiprnoswiping/swipr/MapActivity.java
+++ app/src/main/java/net/swiprnoswiping/swipr/MapActivity.java
@@ -1,24 +1,39 @@
 package net.swiprnoswiping.swipr;
-
+//import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MapActivity extends AppCompatActivity {
-
+    private static String secret_key = "randomSecretConfirmationKey";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
     public void cancelButton(View view){
         Intent cancelIntent = new Intent(this, HomePage.class);
         startActivity(cancelIntent);
-    }
-    public void scanButton(View view){
-        Intent scanIntent = new Intent(this, ScanActivity.class);
-        startActivity(scanIntent);
     }
 
     public void genQR(View view){
@@ -30,4 +45,14 @@ public class MapActivity extends AppCompatActivity {
         Intent mapIntent = new Intent(this, MapsActivity.class);
         startActivity(mapIntent);
     }
+
+    public void scanButton(View view){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+        integrator.setPrompt("Scan the QR Code on the recipient's device");
+        integrator.setBeepEnabled(true);
+        integrator.setBarcodeImageEnabled(true);
+        integrator.initiateScan();
+    }
 }
+
