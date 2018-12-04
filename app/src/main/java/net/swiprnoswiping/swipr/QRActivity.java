27f9dcbb9046b1b16f505f78c89e361e5d27d288
Parina <time-is-immortal@users.noreply.github.com>
2018-12-04 00:56:03 -0500

    Attempting to add QR dependencies

--- app/src/main/java/net/swiprnoswiping/swipr/QRActivity.java
+++ app/src/main/java/net/swiprnoswiping/swipr/QRActivity.java
@@ -1,9 +1,15 @@
 package net.swiprnoswiping.swipr;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import android.widget.ImageView;
+import com.google.zxing.BarcodeFormat;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
 
 public class QRActivity extends AppCompatActivity {
 
@@ -12,6 +18,18 @@ public class QRActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
     }
+
+    try {
+        String random_string = "randomSecretConfirmationKey"
+        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+        Bitmap bitmap = barcodeEncoder.encodeBitmap(random_string, BarcodeFormat.QR_CODE, 400, 400);
+        ImageView imageViewQrCode = (ImageView) findViewById(R.id.qrCode);
+        imageViewQrCode.setImageBitmap(bitmap);
+    } catch(Exception e) {
+
+    }
+
+
 
     public void backButton(View view){
         Intent backIntent = new Intent(this, MapActivity.class);
